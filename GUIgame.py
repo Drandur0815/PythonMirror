3b149e32f3af0d13e96c2e8fd6acdb3e3e8d199d
Drandur0815 <atm88@gmx.de>
2020-06-18 12:30:39 +0200

    changed the apperance of the screen

--- GUIgame.py
+++ GUIgame.py
@@ -50,40 +50,59 @@ mouse = pygame.image.load(mousefile).convert_alpha()
 
 #BILDER LADEN:
 bilder = []
-im01d = pygame.image.load('Images/01d.png')
+im01db = pygame.image.load('Images/01d.png')
+im01d = pygame.transform.scale(im01db, (50, 50))
 bilder.append(im01d)
-im01n = pygame.image.load('Images/01n.png')
+im01nb = pygame.image.load('Images/01n.png')
+im01n = pygame.transform.scale(im01nb, (50, 50))
 bilder.append(im01n)
-im02d = pygame.image.load('Images/02d.png')
+im02db = pygame.image.load('Images/02d.png')
+im02d = pygame.transform.scale(im02db, (50, 50))
 bilder.append(im02d)
-im02n = pygame.image.load('Images/02n.png')
+im02nb = pygame.image.load('Images/02n.png')
+im02n = pygame.transform.scale(im02nb, (50, 50))
 bilder.append(im02n)
-im03d = pygame.image.load('Images/03d.png')
+im03db = pygame.image.load('Images/03d.png')
+im03d = pygame.transform.scale(im03db, (50, 50))
 bilder.append(im03d)
-im03n = pygame.image.load('Images/03n.png')
+im03nb = pygame.image.load('Images/03n.png')
+im03n = pygame.transform.scale(im03nb, (50, 50))
 bilder.append(im03n)
-im04d = pygame.image.load('Images/04d.png')
+im04db = pygame.image.load('Images/04d.png')
+im04d = pygame.transform.scale(im04db, (50, 50))
 bilder.append(im04d)
-im04n = pygame.image.load('Images/04n.png')
+im04nb = pygame.image.load('Images/04n.png')
+im04n = pygame.transform.scale(im04nb, (50, 50))
 bilder.append(im04n)
-im09d = pygame.image.load('Images/09d.png')
+im09db = pygame.image.load('Images/09d.png')
+im09d = pygame.transform.scale(im09db, (50, 50))
 bilder.append(im09d)
-im10d = pygame.image.load('Images/10d.png')
+im10db = pygame.image.load('Images/10d.png')
+im10d = pygame.transform.scale(im10db, (50, 50))
 bilder.append(im10d)
-im10n = pygame.image.load('Images/10n.png')
+im10nb = pygame.image.load('Images/10n.png')
+im10n = pygame.transform.scale(im10nb, (50, 50))
 bilder.append(im10n)
-im11d = pygame.image.load('Images/11d.png')
+im11db = pygame.image.load('Images/11d.png')
+im11d = pygame.transform.scale(im11db, (50, 50))
 bilder.append(im11d)
-im11n = pygame.image.load('Images/11n.png')
+im11nb = pygame.image.load('Images/11n.png')
+im11n = pygame.transform.scale(im11nb, (50, 50))
 bilder.append(im11n)
-im13d = pygame.image.load('Images/13d.png')
+im13db = pygame.image.load('Images/13d.png')
+im13d = pygame.transform.scale(im13db, (50, 50))
 bilder.append(im13d)
-im13n = pygame.image.load('Images/13n.png')
+im13nb = pygame.image.load('Images/13n.png')
+im13n = pygame.transform.scale(im13nb, (50, 50))
 bilder.append(im13n)
-im50d = pygame.image.load('Images/50d.png')
+im50db = pygame.image.load('Images/50d.png')
+im50d = pygame.transform.scale(im50db, (50, 50))
 bilder.append(im50d)
-im50n = pygame.image.load('Images/50n.png')
+im50nb = pygame.image.load('Images/50n.png')
+im50n = pygame.transform.scale(im50nb, (50, 50))
 bilder.append(im50n)
+
+
 
 bilder_dict = {
     "01d" : 0,
@@ -118,9 +137,26 @@ font2 = pygame.font.Font('Roboto-Light.ttf',15)
     
 textX = 5
 textY = 5
-textY2 = 30
-notesX = 1400
-notesY = 300
+textY2 = 1000
+notesX = 800
+notesY = 1500
+
+
+
+#def screensaver():
+#    while True:
+#
+#	screen.fill(0,0,0))
+#
+#	for event in pygame.event.get():
+#	    if event.type == QUIT:
+#		pygame.quit()
+#		sys.exit()
+#            elif event.type == pygame.KEYDOWN and event.key == 27:
+#            	pygame.quit()
+#            	sys.exit()
+
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -128,17 +164,28 @@ def on_connect(client, userdata, flags, rc):
 
 gesture = ''
 day = 0
+screensaver = False
+screen_timer = time.time() + 60
 
 def on_message(client, userdata, msg):
     global day
+    global screensaver
+    global screen_timer
     gesture = str(msg.payload) 
     print(gesture)
-    if 'RIGHT' in gesture:
-	day = (day + 1) % 5 
-	print(day)
-    elif 'LEFT' in gesture:
-	day = (day - 1) % 5
-	print(day)
+    if not 'NONE' in gesture:
+	screen_time = time.time() + 60
+    if 'FAR' in gesture:
+	screensaver = False
+    elif 'NEAR' in gesture:
+	screensaver = True
+    if not screensaver:
+        if 'RIGHT' in gesture:
+	    day = (day + 1) % 5 
+	    print(day)
+        elif 'LEFT' in gesture:
+	    day = (day - 1) % 5
+	    print(day)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -148,54 +195,59 @@ client.connect("192.168.178.222", 1883, 60)
 def show_times(x,y):
     for i in range(0, len(uhrzeiten)):
 	uhrz = font1.render(str(uhrzeiten[i]), True, (150,250,250))	
-	screen.blit(uhrz, (x + i*100 - 10, y + 50))
+	screen.blit(uhrz, (x + i*100 - 10, y + 120))
 	uhr = font2.render("Uhr" ,True, (150,250,250))
-	screen.blit(uhr, ((x + i*100 - 10) + 35, y + 50))
+	screen.blit(uhr, ((x + i*100 - 10) + 35, y + 120))
 
 def show_temps(x,y):
     for i in range(0, len(temps_u_graph)):
 	tug = font1.render(str(temps_u_graph[i]), True, (150,250,250))	
-	screen.blit(tug, (x + i*100 - 10, y + 100))
+	screen.blit(tug, (x + i*100 - 10, y + 30))
 	grad = font2.render("C" ,True, (150,250,250))
-	screen.blit(grad, ((x + i*100 - 10) + 35, y + 100))
+	screen.blit(grad, ((x + i*100 - 10) + 35, y + 30))
 
 def show_current_temp(x,y):
     currtemp = font1.render(str(temps_u_graph[0]), True, (150,250,250))
-    screen.blit(currtemp, (x, y - 230))
+    screen.blit(currtemp, (x + 170, y))
     grad2 = font2.render("C" ,True, (150,250,250))
-    screen.blit(grad2, (x + 30, y - 230))
-    screen.blit(bilder[bilder_dict[icon_graph[0]]], (x + 50, y - 300)) 
+    screen.blit(grad2, (x + 250, y))
+    screen.blit(bilder[bilder_dict[icon_graph[0]]], (x + 270, y)) 
 #    screen.blit(bilder[bilder_dict[icon_graph[i]]], (origin[0] + i*100-10, origin[1] + 300))
 
 def show_temp_tage(x,y):
     for i in range(0, 4):
 	temp_tag = font1.render(str(temp_tage[i]), True, (150,250,250))
-	screen.blit(temp_tag, (x + 600, y + i*50))
+	screen.blit(temp_tag, (x + 200, y + 300 + i*50))
         grad3 = font2.render("C" ,True, (150,250,250))
-        screen.blit(grad3, (x + 650, y + i*50))
-	screen.blit(bilder[bilder_dict[icon_tage[i]]], (x + 700, y + i*50))
+        screen.blit(grad3, (x + 250, y + 300 + i*50))
+	screen.blit(bilder[bilder_dict[icon_tage[i]]], (x + 300, y + 300 + i*50))
 
 def show_messages(x,y):
     for i in range(0, 3):
-        message = font1.render("From : " + mess1Val[i], True, (150,250,250))
+        message = font1.render(mess1Val[i], True, (150,250,250))
         screen.blit(message, (x, y + i*50))
+    ueberschr = font1.render("Emails", True, (150,250,250))
+    screen.blit(ueberschr, (x, y - 55))
     #message2 = font.render("Subject : " + str(messSend), True, (255,255,255))
     #screen.blit(message2, (x, y+150))
     
 def show_datetime(x,y):
     hour = font1.render(str(hourValue), True, (150,250,250))
-    screen.blit(hour, (x+380, y))
+    screen.blit(hour, (x+985, y))
     minute = font1.render(" : " + minVal, True, (150,250,250))
-    screen.blit(minute, (x+410, y))
+    screen.blit(minute, (x+1015, y))
 
 def show_Weekday(x, y):
     weekday1 = font1.render(str(weekday), True, (150,250,250))
-    screen.blit(weekday1, (x, y - 260))
+    screen.blit(weekday1, (x, y))
+    screen.blit(weekday1, (x + 900, y + 35))
 
 def show_Weekdays(x, y):
     for i in range(0, 4):
 	wkds = font1.render(str(weekdays[i]), True, (150,250,250))
-	screen.blit(wkds, (x + 500, y + i*50))
+	screen.blit(wkds, (x, y + 300 + i*50))
+    ueberschr2 = font1.render("Woche", True, (150,250,250))
+    screen.blit(ueberschr2, (x, y + 250))
 
 def show_notes(x, y):
     head1 = font1.render(titel, True, (150,250,250))
@@ -252,7 +304,8 @@ while True:
  
     #BEFEHLE WEMOS
     client.loop()
-
+    if zeit >= screen_timer:
+	screensaver = True
 
     #UNLOCK MIRROR
 #    notelist_scr = sn.get_note_list(data = False, since = None)
@@ -321,7 +374,7 @@ while True:
 	color = (150,250,250)
 #	start_pos = (500,500)
 #	end_pos = (700,450)
-	origin = (500, 500)
+	origin = (5, 200)
 #	points = [(500, 500), (700, 400), (800, 450), (1000, 500)]
 #	uhrzeiten = []
 	temps_u_graph = []
@@ -427,8 +480,11 @@ while True:
         pygame.draw.lines(screen, color, False, points)
     show_times(origin[0], origin[1])
     show_temps(origin[0], origin[1])
-    show_current_temp(origin[0], origin[1])
+    show_current_temp(origin[0], textY)
     show_temp_tage(origin[0], origin[1])
+
+    pygame.draw.line(screen, color, (textX, textY+480), (textX+500, textY+480))
+
     #BILDER ZEIGEN:
 #    screen.blit(im01d, (1300, 200))
 #    screen.blit(im01n, (1300, 250))
@@ -451,7 +507,7 @@ while True:
     #BILDER UNTERM GRAPH:
 #    im = ['im']
     for i in range(0, len(icon_graph)):
-	screen.blit(bilder[bilder_dict[icon_graph[i]]], (origin[0] + i*100-10, origin[1] + 300))
+	screen.blit(bilder[bilder_dict[icon_graph[i]]], (origin[0] + i*100-10, origin[1] + 70))
 #    print("".join((im[0], icon_graph[0])))
         
     if zeit >= mail_timer:
@@ -487,6 +543,7 @@ while True:
 
             mail_timer = zeit + 60*5
     
+    pygame.draw.line(screen, color, (textX, textY2-20), (textX+700, textY2-20))
     show_messages(textX, textY2)
 
 #    mail = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -565,7 +622,7 @@ while True:
         weekday = 'Sonntag'
 
     #print(weekday)
-    show_Weekday(origin[0], origin[1])
+    show_Weekday(origin[0], textY)
 
     #WEEKDAYS FOR THE FORECAST
     for i in range(1, 5):
@@ -619,7 +676,11 @@ while True:
     elif minuteValue >= 10:
         minVal = str(minuteValue)
     
+    pygame.draw.line(screen, color, (textX+800, textY+35), (textX+1200, textY+35))
     show_datetime(textX, textY)
+
+    if screensaver:
+	screen.blit(background_surface, (0,0))
     
     # finish off by updating our display
     pygame.display.update()
